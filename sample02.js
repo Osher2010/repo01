153899175124c49cc5f81665878044e5077d8bd3
Osher2010 <53093052+Osher2010@users.noreply.github.com>
2023-06-09 17:42:58 +0300

    Create sample02.js

---
+++ sample02.js
@@ -0,0 +1,14 @@
+// Function to test
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Testing the function
+console.log(addNumbers(5, 10)); // Output: 15
+console.log(addNumbers(-2, 8)); // Output: 6
+console.log(addNumbers(3.14, 2.86)); // Output: 6
+
+// Additional test cases
+console.log(addNumbers(0, 0)); // Output: 0
+console.log(addNumbers(100, -100)); // Output: 0
+console.log(addNumbers(1.5, -1.2)); // Output: 0.3
